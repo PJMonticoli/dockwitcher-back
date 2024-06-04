339565817892d508eda6fdfb6cd7f260cf2e985d
PJMonticoli <pablo_monticoli@hotmail.com>
2024-06-04 16:09:51 -0300

    WIP implemento mapeo nueva tractora. Falta trasladarlo al modelmapper

--- src/main/kotlin/com/hotakakademy/dockwitcher/domain/factories/TractoraFactory.kt
+++ src/main/kotlin/com/hotakakademy/dockwitcher/domain/factories/TractoraFactory.kt
@@ -1,19 +1,25 @@
 package com.hotakakademy.dockwitcher.domain.factories
 import com.hotakakademy.dockwitcher.DTO.TractoraDto
+import com.hotakakademy.dockwitcher.domain.entities.Conductor
 import com.hotakakademy.dockwitcher.domain.entities.Tractora
 import com.hotakakademy.dockwitcher.domain.repositories.ITractoraRepository
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Component
 
 @Component
-class TractoraFactory (
+class TractoraFactory(
     private val repository: ITractoraRepository,
     private val modelMapper: ModelMapper
-): ITractoraFactory {
+) : ITractoraFactory {
+    // MAPEO TEMPORAL
     override fun createFromDto(tractoraDto: TractoraDto): Tractora {
         val tractora = Tractora()
         tractora.setRepository(repository)
-        modelMapper.map(tractoraDto, tractora)
+        tractora.matricula = tractoraDto.matricula
+        tractora.modelo.marca = tractoraDto.marca
+        tractora.caracteristicas.carga.pma = tractoraDto.pma ?: 0
+        tractora.conductor = Conductor().apply { nombre = tractoraDto.nombre }
+
         return tractora
     }
 }
